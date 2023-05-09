d53c8e254b8ebe5366d8ef4e44bc25dd840f8924
David Martinez <davidmartinez3601@gmail.com>
2023-05-08 21:53:04 -0300

    fix services

--- src/services/localidad/localidad.service.ts
+++ src/services/localidad/localidad.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'enviroment';
+import { environment } from 'src/enviroments/enviroment';
 import { Localidad, Provincia } from 'src/models/models';
 
 @Injectable({
--- src/services/productos/producto.service.ts
+++ src/services/productos/producto.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'enviroment';
 import { Observable } from 'rxjs';
+import { environment } from 'src/enviroments/enviroment';
 import { Producto, ResponseHttp } from 'src/models/models';
 
 @Injectable({
