73a7052ff5023712c903e3792f2d1e5aaaa2f252
David Martinez <davidmartinez3601@gmail.com>
2023-08-01 20:17:47 -0300

    evnn

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://utn-final-java-backend-production-6f05.up.railway.app'
+    apiUrl: 'https://utn-final-java-backend-production.up.railway.app'
 };
